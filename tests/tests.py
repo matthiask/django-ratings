029c8ddfd0d8a3645970b1aaaf02e3675c7e6c20
Jim Graham <james.a.graham+gh@gmail.com>
2017-12-12 15:43:40 -0500

    cirle 7

--- tests/tests.py
+++ tests/tests.py
@@ -6,9 +6,8 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.conf import settings
 
-from exceptions import *
-from models import Vote, SimilarUser, IgnoredObject
-from fields import AnonymousRatingField, RatingField
+from djangoratings.models import Vote, SimilarUser, IgnoredObject
+from djangoratings.fields import AnonymousRatingField, RatingField
 
 settings.RATINGS_VOTES_PER_IP = 1
 
@@ -62,6 +61,7 @@ class RatingTestCase(unittest.TestCase):
         self.assertEquals(instance.rating2.score, 0)
         self.assertEquals(instance.rating2.votes, 0)
 
+
 class RecommendationsTestCase(unittest.TestCase):
     def setUp(self):
         self.instance = RatingTestModel.objects.create()
@@ -69,11 +69,11 @@ class RecommendationsTestCase(unittest.TestCase):
         self.instance3 = RatingTestModel.objects.create()
         self.instance4 = RatingTestModel.objects.create()
         self.instance5 = RatingTestModel.objects.create()
-        
+
         # Test users
         self.user = User.objects.create(username=str(random.randint(0, 100000000)))
         self.user2 = User.objects.create(username=str(random.randint(0, 100000000)))
-    
+
     def testExclusions(self):
         Vote.objects.all().delete()
 
@@ -91,9 +91,9 @@ class RecommendationsTestCase(unittest.TestCase):
 
         recs = list(SimilarUser.objects.get_recommendations(self.user2, RatingTestModel))
         self.assertEquals(len(recs), 4)
-        
+
         ct = ContentType.objects.get_for_model(RatingTestModel)
-        
+
         IgnoredObject.objects.create(user=self.user2, content_type=ct, object_id=self.instance2.pk)
 
         recs = list(SimilarUser.objects.get_recommendations(self.user2, RatingTestModel))
@@ -105,11 +105,11 @@ class RecommendationsTestCase(unittest.TestCase):
         recs = list(SimilarUser.objects.get_recommendations(self.user2, RatingTestModel))
         self.assertEquals(len(recs), 1)
         self.assertEquals(recs, [self.instance5])
-        
+
         self.instance5.rating.add(score=1, user=self.user2, ip_address='127.0.0.2')
         recs = list(SimilarUser.objects.get_recommendations(self.user2, RatingTestModel))
         self.assertEquals(len(recs), 0)
-    
+
     def testSimilarUsers(self):
         Vote.objects.all().delete()
 
@@ -121,14 +121,14 @@ class RecommendationsTestCase(unittest.TestCase):
         self.instance.rating.add(score=1, user=self.user2, ip_address='127.0.0.2')
         self.instance2.rating.add(score=1, user=self.user2, ip_address='127.0.0.2')
         self.instance3.rating.add(score=1, user=self.user2, ip_address='127.0.0.2')
-        
+
         SimilarUser.objects.update_recommendations()
 
         self.assertEquals(SimilarUser.objects.count(), 2)
 
         recs = list(SimilarUser.objects.get_recommendations(self.user2, RatingTestModel))
         self.assertEquals(len(recs), 2)
-        
+
         self.instance4.rating.add(score=1, user=self.user2, ip_address='127.0.0.2')
 
         SimilarUser.objects.update_recommendations()
