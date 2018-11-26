3268b5d03ef7028c15340c10bdcfc36cf395bf0e
Matthias Kestenholz <mk@feinheit.ch>
2018-11-26 15:00:26 +0100

    Not a callable anymore in newest Django versions

--- djangoratings/fields.py
+++ djangoratings/fields.py
@@ -104,7 +104,7 @@ class RatingManager(object):
             key=self.field.key,
         )
 
-        if not (user and user.is_authenticated()):
+        if not (user and user.is_authenticated):
             if not ip_address:
                 raise ValueError('``user`` or ``ip_address`` must be present.')
             kwargs['user__isnull'] = True
@@ -151,7 +151,7 @@ class RatingManager(object):
         if score < 0 or score > self.field.range:
             raise InvalidRating("%s is not a valid choice for %s" % (score, self.field.name))
 
-        is_anonymous = (user is None or not user.is_authenticated())
+        is_anonymous = (user is None or not user.is_authenticated)
         if is_anonymous and not self.field.allow_anonymous:
             raise AuthRequired("user must be a user, not '%r'" % (user,))
 
